55af963ed27fcbe36d7cb964a1b95c4d1e372c6e
Mark Blunk <mblunk@gmail.com>
2020-01-28 13:15:01 -0800

    cleanup

--- system/.completion.bash
+++ system/.completion.bash
@@ -1,2 +1,9 @@
 #enable bash completion
-source /etc/profile.d/bash_completion.sh
+# source /etc/profile.d/bash_completion.sh
+. /etc/bash_completion
+
+# for some reason I need to add these manually. I think I am doing something wrong
+source /usr/share/bash-completion/completions/apt
+source /usr/share/bash-completion/completions/docker
+source /usr/share/bash-completion/completions/maven
+source /usr/share/bash-completion/completions/make
