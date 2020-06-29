869f541c82e5a63d76c35a2de58281ae9c2b4ab6
Mark Blunk <mblunk@gmail.com>
2020-06-29 16:31:23 -0700

    stuff

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 
 # Get current dir (so run this script from anywhere)
 
-export DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+export DOTFILES_DIR=$(dirname $0)
 # Bunch of symlinks
 
 ln -sfv "$DOTFILES_DIR/runcom/.bash_profile" $HOME
--- system/.completion.bash
+++ system/.completion.bash
@@ -5,5 +5,6 @@
 # for some reason I need to add these manually. I think I am doing something wrong
 source /usr/share/bash-completion/completions/apt
 source /usr/share/bash-completion/completions/docker
+source /usr/share/bash-completion/completions/java
 source /usr/share/bash-completion/completions/maven
 source /usr/share/bash-completion/completions/make
