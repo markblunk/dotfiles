d6c6bc3f5f45bdd7f75bd0a43444a709d6c825f4
Mark Blunk <mblunk@gmail.com>
2022-11-11 16:08:29 -0800

    remove some completions

--- system/.completion.bash
+++ system/.completion.bash
@@ -7,13 +7,11 @@ eval "$(stack --bash-completion-script stack)"
 
 # for some reason I need to add these manually. I think I am doing something wrong
 source /usr/share/bash-completion/completions/apt
-source /usr/share/bash-completion/completions/clang-10
 source /usr/share/bash-completion/completions/chmod
 source /usr/share/bash-completion/completions/cmake
 source /usr/share/bash-completion/completions/docker
 source /usr/share/bash-completion/completions/gcc
 source /usr/share/bash-completion/completions/java
-source /usr/share/bash-completion/completions/maven
 source /usr/share/bash-completion/completions/make
 source /usr/share/bash-completion/completions/openssl
 source /usr/share/bash-completion/completions/ssh-add
