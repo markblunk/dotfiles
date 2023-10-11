bdefb02b8072ce2247ae50782ce4c7a5b055ee0e
Mark Blunk <mblunk@gmail.com>
2023-10-11 16:00:16 -0700

    use internal copy of use-package

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,24 +1,8 @@
 ;;; package --- initialization file.
 ;;; Commentary:
-;; Set up the package archives, download use-package, and use the file
-;; emacs.org to generate the corresponding emacs.el file
+;; Set up the package archives and use the file emacs.org to generate the
+;; corresponding emacs.el file
 
 ;;; Code:
-
-(require 'package)
-
-(setq package-enable-at-startup nil
-      package-archives '(("nongnu"   . "https://elpa.nongnu.org/nongnu/")
-                         ("gnu"   . "https://elpa.gnu.org/packages/")
-                         ("melpa" . "https://melpa.org/packages/")))
-
-(when (< emacs-major-version 27)
-  (package-initialize))
-
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
-
-(require 'use-package)
 (org-babel-load-file "~/.emacs.d/emacs.org")
 ;;; init.el ends here
