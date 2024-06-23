25620a1ec0fe2fc52bf2700c8bc612ab2f4efc0e
Mark Blunk <mblunk@gmail.com>
2024-06-23 09:35:43 -0700

    stuff

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -4,5 +4,11 @@
 ;; corresponding emacs.el file
 
 ;;; Code:
+(when (< emacs-major-version 29)
+  (unless (package-installed-p 'use-package)
+    (package-refresh-contents)
+    (package-install 'use-package))
+  (require 'use-package))
+
 (org-babel-load-file "~/.emacs.d/emacs.org")
 ;;; init.el ends here
